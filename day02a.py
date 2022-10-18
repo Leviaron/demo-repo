8f1701111870a3f37247e2141a63692319c90b19
sajttorta <fitymaszukulet@jm.com>
2022-10-18 09:19:27 +0200

    Added day02

---
+++ day02a.py
@@ -0,0 +1,20 @@
+import json
+
+
+stuff = {
+    "name" : "Jakab",
+    "age" : 30,
+    "married" : True,
+    "children" : ("Anna", "Billy"),
+    "cars" : [
+        
+           {"model": "BMW", "type" : "Z3" },
+        { "model" : "Audi", "type": "S8"},
+    ],
+    "pets" : None
+},
+
+data = json.dumps(stuff, indent=3, sort_keys=True)
+file = open("stuff.json", "w")
+file.write(data)
+file.close()
